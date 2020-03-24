743cba61b23bb3ca356881c4fa8421dd573ea60f
Yair Facio <yair.facio11@gmail.com>
2020-03-24 12:21:14 -0600

    Installed Font Awesome

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def new
-    redirect_to signup_path unless !logged_in?
+    # redirect_to signup_path unless !logged_in?
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
