7ff2500ef83dfb1dae20b5b09dcc82697ac8505b
Yair Facio <yair.facio11@gmail.com>
2020-03-26 15:47:10 -0600

    Fixing height and added validation

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,6 +2,6 @@ class Purchase < ApplicationRecord
   belongs_to :creator, foreign_key: :author_id, class_name: "User"
   belongs_to :group, optional: true
 
-  validates :name, presence: true, length: {minimum:3, maximum:30}
+  validates :name, presence: true, length: {minimum:3, maximum:20}
   validates :amount, presence: true
 end
