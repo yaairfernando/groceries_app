891ec5e17c865b01a24f4631add5196087850bcd
Yair Facio <yair.facio11@gmail.com>
2020-03-28 12:29:03 -0600

    Finished Purchases Integration specs

--- spec/integration/purchases_spec.rb
+++ spec/integration/purchases_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 feature 'Purchases' do
@@ -9,25 +11,78 @@ feature 'Purchases' do
     click_link 'Dashboard'
     li = find_all('li')
     expect(li.count).to eq(3)
+  end
+
+  scenario 'Create a new transaction' do
+    click_link 'All groups'
+    create_group
+    first('.menu__icon > a').click
     click_link 'All my transactions'
-  end
-
-  scenario 'Visit index' do
-    expect(page).to have_content('Transactions')
-  end
-
-  scenario 'Create a new group' do
     click_link 'Register new Purchase'
     expect(find('form')).to be_present
-    # expect(find('#group_icon')).to be_present
     within('form') do
       fill_in 'purchase_name', with: 'Potatoes'
       fill_in 'purchase_amount', with: 15
-      fill_in 'purchase_group_id', with: find('#purchase_group_id').first(:option).select_option
     end
-    # find('#group_icon').set(Rails.root.join('spec/support/candy.png'))
+    find('select > option:nth-child(2)').select_option
     click_button 'Create'
-    expect(page).to have_content('A new group has been added!!')
+    expect(find('.purchase__image > img')).to be_present
+    expect(page).to have_content('Your transaction has been added!!')
+  end
+end
+
+feature 'Purchases' do
+  include Integration
+  before(:each) do
+    sign_up
+    sign_out
+    log_in
+    click_link 'Dashboard'
+    li = find_all('li')
+    expect(li.count).to eq(3)
   end
 
+  scenario 'Create a new purchase without adding a group' do
+    click_link 'All groups'
+    create_group
+    first('.menu__icon > a').click
+    click_link 'All my transactions'
+    click_link 'Register new Purchase'
+    expect(find('form')).to be_present
+    within('form') do
+      fill_in 'purchase_name', with: 'Potatoes'
+      fill_in 'purchase_amount', with: 15
+    end
+    click_button 'Create'
+    expect(page).to have_content('Your transaction has been added!!')
+  end
+end
+
+feature 'Purchases' do
+  include Integration
+  before(:each) do
+    sign_up
+    sign_out
+    log_in
+    click_link 'Dashboard'
+    li = find_all('li')
+    expect(li.count).to eq(3)
+  end
+
+  scenario 'Visit index' do
+    click_link 'All my transactions'
+    expect(page).to have_content('Transactions')
+  end
+
+  scenario 'Renders the new template again' do
+    click_link 'All my transactions'
+    click_link 'Register new Purchase'
+    expect(find('form')).to be_present
+    within('form') do
+      fill_in 'purchase_name', with: ''
+      fill_in 'purchase_amount', with: 15
+    end
+    click_button 'Create'
+    expect(page).to have_content("can't be blank")
+  end
 end
