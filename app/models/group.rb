0f0026f6d46b15fada644e4674b46d34271f28e1
Yair Facio <yair.facio11@gmail.com>
2020-03-28 15:33:47 -0600

    Fixing extra space

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,7 +4,7 @@ class Group < ApplicationRecord
   belongs_to :creator, foreign_key: :creator_id, class_name: 'User'
   has_many :purchases, dependent: :destroy
 
-  validates :name, presence: true, length: { minimum: 3, maximum: 15 }, uniqueness:  true
+  validates :name, presence: true, length: { minimum: 3, maximum: 15 }, uniqueness: true
   validates :icon, presence: true
   # mount_uploader :icon, IconUploader
 end
