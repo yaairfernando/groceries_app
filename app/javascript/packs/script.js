984d4b5e00d98520a08cc2c1cffe6869f61f7938
Yair Facio <yair.facio11@gmail.com>
2020-04-01 11:26:47 -0600

    Added styles for mobile in users dashboard

--- app/javascript/packs/script.js
+++ app/javascript/packs/script.js
@@ -1,16 +1,16 @@
-import $ from "jquery";
+// import $ from "jquery";
 
 
-$(() => {
-  if ($('.notification').length) {
-    setInterval(() => {
-      $('.notification').attr('style', 'display: none');
-    }, 5000)
-  }
+// $(() => {
+//   if ($('.notification').length) {
+//     setInterval(() => {
+//       $('.notification').attr('style', 'display: none');
+//     }, 5000)
+//   }
 
-  if ($('span.error').length) {
-    setInterval(() => {
-      $('span.error').attr('style', 'display: none');
-    }, 5000)
-  }
-})
+//   if ($('span.error').length) {
+//     setInterval(() => {
+//       $('span.error').attr('style', 'display: none');
+//     }, 5000)
+//   }
+// })
